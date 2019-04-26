fdae7e17da2ab35e6215823987ce4f65aec0986d
Erik Andersson <erik_andersson007@hotmail.com>
2019-04-26 16:31:16 +0200

    now gets game info and prints to console on request

--- main.js
+++ main.js
@@ -2,8 +2,21 @@ var calenderLink = document.createElement("a");
 var linkText     = document.createTextNode(chrome.i18n.getMessage("calendar_link"));
 
 calenderLink.className = "calendar";
+calenderLink.setAttribute('href','#');
+calenderLink.onclick = getGameInfo;
 calenderLink.appendChild(linkText);
 
-var shareBox = document.querySelector(".share-box .box-share");
+var gameInfo = document.querySelector(".game-info-page");
+var shareBox = gameInfo.querySelector(".share-box .box-share");
 
 shareBox.appendChild(calenderLink);
+
+function getGameInfo() {
+	var homeTeam = gameInfo.querySelector(".home .name").firstChild.nodeValue;
+	var awayTeam = gameInfo.querySelector(".away .name").firstChild.nodeValue;
+	var startTime = gameInfo.querySelector(".start-time span span");
+	var date = startTime.firstChild.nodeValue;
+	var kickOff = startTime.lastChild.nodeValue;
+	var stadium = gameInfo.querySelector(".arena-info h3").firstChild.nodeValue;
+	console.log(homeTeam + ' - ' + awayTeam + ' @ ' + stadium +' '+ date + ' ' + kickOff);
+}
