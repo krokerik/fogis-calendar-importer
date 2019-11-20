2647e7b73497e5c14ee9226cd84907dff5b2f2bd
Erik Andersson <erik_andersson007@hotmail.com>
2019-11-20 22:08:00 +0100

    localised options popup

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 chrome.storage.sync.get(['calType'], function(data) {
 	document.options.calType.value = data.calType;
 });
+document.forms.options.calType[0].nextSibling.textContent = chrome.i18n.getMessage("ics_option");
+document.forms.options.calType[1].nextSibling.textContent = chrome.i18n.getMessage("google_option");
 
 document.forms.options.addEventListener('change', function(e) {
 	if(e.target.name === 'calType') {
