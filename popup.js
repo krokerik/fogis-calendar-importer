767068ee09bbbcff482283d9fc8d8807fe09b891
Erik Andersson <erik_andersson007@hotmail.com>
2019-11-20 21:30:23 +0100

    remove traces of background colour example

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get(['color'], function(data) {
-	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', data.color);
-});
-changeColor.onclick = function(element) {
-	let color = element.target.value;
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{code: 'document.body.style.backgroundColor = "' + color + '";'});
-	});
-};
-
 chrome.storage.sync.get(['calType'], function(data) {
 	document.options.calType.value = data.calType;
 });
